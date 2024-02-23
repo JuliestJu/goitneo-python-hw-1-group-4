3cc6c35159af0d1082615efc580a38809f65b300
Julia Vorotchenko <juliavorotchenko@gmail.com>
2024-02-23 12:39:01 +0200

    finish base for bot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from Birthday_Helper.birthday_helper import get_birthdays_per_week
-from Bot import bot
+from Bot.bot import AssistantBot
 
 #Task_1
 users = [
@@ -12,22 +12,28 @@ users = [
 
 # Call the function from birthday_helper.py
 get_birthdays_per_week(users)
-#Task2
 
+#Task2
 def main():
-    contacts = {}
+    bot = AssistantBot()
     print("Welcome to the assistant bot!")
     while True:
         user_input = input("Enter a command: ")
-        command, *args = bot.parse_input(user_input)
+        command, args = bot.parse_input(user_input)
 
         if command in ["close", "exit"]:
-            print("Good bye!")
+            print("Goodbye!")
             break
         elif command == "hello":
             print("How can I help you?")
         elif command == "add":
-            print(bot.add_contact(args, contacts))
+            print(bot.add_contact(args))
+        elif command == "change":
+            print(bot.change_contact_phone(args))
+        elif command == "phone":
+            print(bot.display_contact_phone(args))
+        elif command == "all":
+            print(bot.display_all_contacts())
         else:
             print("Invalid command.")
 
